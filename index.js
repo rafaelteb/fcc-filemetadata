893b81ffb21fc933bfbedd58fff1986c417afc53
R.A. te Boekhorst <rateboekhorst@mailbox.org>
2024-06-01 18:09:31 +0200

    feat: add required responses... but no save to db yet

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const Grid = require('gridfs-stream');
+const crypto = require('crypto');
+const path = require('path');
+
 
 const app = express();
 
@@ -13,6 +18,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(fileUpload());
 
 // Connect to MongoDB
 async function connectToDatabase() {
@@ -34,13 +40,46 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+const conn = mongoose.connection;
+
+// Initialize GridFS stream
+let gfs;
+conn.once('open', () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
 // Serve static files
 app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Post and respond with file infos
+app.post('/api/fileanalyse', async (req, res) => {
+    try {
+      if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({ error: 'No files were uploaded.' });
+      }
+      const file = req.files.upfile;
+      const name = file.name;
+      const type = file.type;
+      const size = file.size;
 
+      // Create the exercise
+      const response = {
+        name: file.name,
+        type: file.mimetype,
+        size: file.size,
+      };
+      
+      // Return the response
+      res.status(201).json(response);
+
+    } catch (error) {
+      
+    }
+})
 
 
 
