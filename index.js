dfa36f74e20f24c2bee6612b1ab8a199f1d4b39e
R.A. te Boekhorst <rateboekhorst@mailbox.org>
2024-06-01 17:34:13 +0200

    feat: addding own setup and mongoose to package.json

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+// Importing necessary libraries
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-var app = express();
+const app = express();
 
+// Basic Configurations
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+// Connect to MongoDB
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to the database');
+  } catch (err) {
+    console.error('Database connection error:', err);
+    process.exit(1);
+  }
+}
+connectToDatabase();
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+// Serve static files
+app.use('/public', express.static(`${process.cwd()}/public`));
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+
+
